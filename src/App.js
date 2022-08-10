ca5b6051dcb8ef28bf965e231362476f8a5076fd
nawan44 <rachmat.d.gunawan@gmail.com>
2022-08-10 14:54:32 +0700

    slice design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './page/Login';
+import Profile from './page/Profile';
+import Layout from './component/view/layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShippingComps from './page/Shipping Comps';
+import AddShippingComps from './page/Add Shipping Comps';
+import Routers from "./router"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  <Routers/>
   );
 }
 
